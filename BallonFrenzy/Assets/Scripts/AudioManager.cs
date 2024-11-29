c3faff11c5b78d085b012febe5352d3da8a1d0a0
Brianna Chaleunsouk <brichal2003@gmail.com>
2024-11-28 22:20:31 -0500

    Add Background Music

---
+++ BallonFrenzy/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audiomanager : MonoBehaviour
+{
+    [Header("-------- Audio Source --------")]
+    [SerializeField] AudioSource MusicSource;
+
+    [Header("-------- Audio Clip --------")]
+    public AudioClip Background;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MusicSource.clip = Background;
+        MusicSource.Play();
+    }
+
+}
